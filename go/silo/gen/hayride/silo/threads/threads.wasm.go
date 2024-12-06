968372da1c67b20373a48cdb633665246e67c498
Ethan M Lewis <ethanlewis787@gmail.com>
2024-12-06 12:20:02 -0800

    feat: update to v0.0.19 (#10)

--- go/silo/gen/hayride/silo/threads/threads.wasm.go
+++ go/silo/gen/hayride/silo/threads/threads.wasm.go
@@ -6,12 +6,12 @@ import (
 	"github.com/bytecodealliance/wasm-tools-go/cm"
 )
 
-// This file contains wasmimport and wasmexport declarations for "hayride:silo@0.0.18".
+// This file contains wasmimport and wasmexport declarations for "hayride:silo@0.0.19".
 
-//go:wasmimport hayride:silo/threads@0.0.18 spawn
+//go:wasmimport hayride:silo/threads@0.0.19 spawn
 //go:noescape
 func wasmimport_Spawn(path0 *uint8, path1 uint32, args0 *string, args1 uint32, result *cm.Result[int32, int32, ErrNo])
 
-//go:wasmimport hayride:silo/threads@0.0.18 wait
+//go:wasmimport hayride:silo/threads@0.0.19 wait
 //go:noescape
 func wasmimport_Wait(threadId0 uint32, result *cm.Result[int32, int32, ErrNo])
