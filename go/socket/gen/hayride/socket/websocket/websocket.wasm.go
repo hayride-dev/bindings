6bdd363be5808633a571648b9764d3eff0e8383a
jeff1010322 <jeff1010322@gmail.com>
2024-12-18 07:33:23 -0500

    feat: update to use coven v0.0.21

--- go/socket/gen/hayride/socket/websocket/websocket.wasm.go
+++ go/socket/gen/hayride/socket/websocket/websocket.wasm.go
@@ -6,10 +6,10 @@ import (
 	"github.com/bytecodealliance/wasm-tools-go/cm"
 )
 
-// This file contains wasmimport and wasmexport declarations for "hayride:socket@0.0.20".
+// This file contains wasmimport and wasmexport declarations for "hayride:socket@0.0.21".
 
-//go:wasmexport hayride:socket/websocket@0.0.20#handle
-//export hayride:socket/websocket@0.0.20#handle
+//go:wasmexport hayride:socket/websocket@0.0.21#handle
+//export hayride:socket/websocket@0.0.21#handle
 func wasmexport_Handle(text0 *uint8, text1 uint32, out0 uint32) {
 	text := cm.LiftString[string]((*uint8)(text0), (uint32)(text1))
 	out := cm.Reinterpret[OutputStream]((uint32)(out0))
