ae8e84066a5f0ab12049a681a014b7db9f727ffb
Jeff Simpson <jeff1010322@gmail.com>
2025-02-26 16:54:23 -0500

    feat: update for coven v0.0.32, implement thread kill (#22)

--- go/ai/gen/exports/hayride/ai/agents/agents.exports.go
+++ go/ai/gen/exports/hayride/ai/agents/agents.exports.go
@@ -6,9 +6,9 @@ import (
 	"go.bytecodealliance.org/cm"
 )
 
-// Exports represents the caller-defined exports from "hayride:ai/agents@0.0.31".
+// Exports represents the caller-defined exports from "hayride:ai/agents@0.0.32".
 var Exports struct {
-	// Error represents the caller-defined exports for resource "hayride:ai/agents@0.0.31#error".
+	// Error represents the caller-defined exports for resource "hayride:ai/agents@0.0.32#error".
 	Error struct {
 		// Destructor represents the caller-defined, exported destructor for resource "error".
 		//
--- go/silo/thread.go
+++ go/silo/thread.go
@@ -26,8 +26,17 @@ func (p *Thread) Spawn(path string, function string, args ...string) (string, er
 func (p *Thread) Wait(threadID string) ([]byte, error) {
 	result := threads.Wait(threadID)
 	if result.IsErr() {
-		return nil, fmt.Errorf("thread error: %v", result.Err())
+		return nil, fmt.Errorf("failed to wait for thread: %v", result.Err())
 	}
 
 	return result.OK().Slice(), nil
+}
+
+func (p *Thread) Kill(threadID string) error {
+	result := threads.Kill(threadID)
+	if result.IsErr() {
+		return fmt.Errorf("failed ot kill thread: %v", result.Err())
+	}
+
+	return nil
 }
