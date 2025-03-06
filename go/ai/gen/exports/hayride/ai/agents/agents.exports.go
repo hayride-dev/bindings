1ec40283b4afaf1d20de9ea62835552263211086
elewis787 <ethanlewis787@gmail.com>
2025-03-06 10:20:33 -0800

    feat: add wasi io writer bindings to get outputstream pointer

--- go/ai/gen/exports/hayride/ai/agents/agents.exports.go
+++ go/ai/gen/exports/hayride/ai/agents/agents.exports.go
@@ -6,9 +6,9 @@ import (
 	"go.bytecodealliance.org/cm"
 )
 
-// Exports represents the caller-defined exports from "hayride:ai/agents@0.0.33".
+// Exports represents the caller-defined exports from "hayride:ai/agents@0.0.34".
 var Exports struct {
-	// Error represents the caller-defined exports for resource "hayride:ai/agents@0.0.33#error".
+	// Error represents the caller-defined exports for resource "hayride:ai/agents@0.0.34#error".
 	Error struct {
 		// Destructor represents the caller-defined, exported destructor for resource "error".
 		//
