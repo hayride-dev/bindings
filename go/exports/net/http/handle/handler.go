51e6173845b451c5c8391c731695234096ec75ae
Ethan M Lewis <ethanlewis787@gmail.com>
2025-05-13 10:30:20 -0700

    feat: add http config and use wasi-confg, update to v0.0.48 (#47)
    * feat: add http config and use wasi-confg, update to v0.0.48
    
    * refactor: update new to export to match export bindings patterns

--- go/exports/net/http/handle/handler.go
+++ go/exports/net/http/handle/handler.go
@@ -28,7 +28,7 @@ func Handler(h http.Handler) {
 
 func wasiHandle(request types.IncomingRequest, responseOut types.ResponseOutparam) {
 	// construct the http.Request and http.ResponseWriter from wasi types
-	req, err := WASItoHTTPRequest(request)
+	req, err := wasiToHTTPRequest(request)
 	if err != nil {
 		fmt.Printf("failed to convert wasi/http/types.IncomingRequest to http.Request: %s\n", err)
 		return
