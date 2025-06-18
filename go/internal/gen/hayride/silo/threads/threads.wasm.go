8af13e28c43560ac28c1addfc796b60d92501995
jeff1010322 <jeff1010322@gmail.com>
2025-06-18 16:46:06 -0400

    feat: update to coven v0.0.54, add model repository use to agent

--- go/internal/gen/hayride/silo/threads/threads.wasm.go
+++ go/internal/gen/hayride/silo/threads/threads.wasm.go
@@ -6,32 +6,32 @@ import (
 	"go.bytecodealliance.org/cm"
 )
 
-// This file contains wasmimport and wasmexport declarations for "hayride:silo@0.0.53".
+// This file contains wasmimport and wasmexport declarations for "hayride:silo@0.0.54".
 
-//go:wasmimport hayride:silo/threads@0.0.53 [resource-drop]thread
+//go:wasmimport hayride:silo/threads@0.0.54 [resource-drop]thread
 //go:noescape
 func wasmimport_ThreadResourceDrop(self0 uint32)
 
-//go:wasmimport hayride:silo/threads@0.0.53 [method]thread.id
+//go:wasmimport hayride:silo/threads@0.0.54 [method]thread.id
 //go:noescape
 func wasmimport_ThreadID(self0 uint32, result *cm.Result[string, string, ErrNo])
 
-//go:wasmimport hayride:silo/threads@0.0.53 [method]thread.wait
+//go:wasmimport hayride:silo/threads@0.0.54 [method]thread.wait
 //go:noescape
 func wasmimport_ThreadWait(self0 uint32, result *cm.Result[cm.List[uint8], cm.List[uint8], ErrNo])
 
-//go:wasmimport hayride:silo/threads@0.0.53 spawn
+//go:wasmimport hayride:silo/threads@0.0.54 spawn
 //go:noescape
 func wasmimport_Spawn(pkg0 *uint8, pkg1 uint32, function0 *uint8, function1 uint32, args0 *string, args1 uint32, result *cm.Result[Thread, Thread, ErrNo])
 
-//go:wasmimport hayride:silo/threads@0.0.53 status
+//go:wasmimport hayride:silo/threads@0.0.54 status
 //go:noescape
 func wasmimport_Status(id0 *uint8, id1 uint32, result *cm.Result[ThreadMetadataShape, ThreadMetadata, ErrNo])
 
-//go:wasmimport hayride:silo/threads@0.0.53 kill
+//go:wasmimport hayride:silo/threads@0.0.54 kill
 //go:noescape
 func wasmimport_Kill(id0 *uint8, id1 uint32, result *cm.Result[ErrNo, struct{}, ErrNo])
 
-//go:wasmimport hayride:silo/threads@0.0.53 group
+//go:wasmimport hayride:silo/threads@0.0.54 group
 //go:noescape
 func wasmimport_Group(result *cm.Result[cm.List[ThreadMetadata], cm.List[ThreadMetadata], ErrNo])
