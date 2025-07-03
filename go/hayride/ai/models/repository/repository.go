8b5836cf73092303727896559b7085a55bc43668
jeff1010322 <jeff1010322@gmail.com>
2025-07-03 09:04:32 -0400

    feat: update to coven v0.0.57, implement new model repository interface

--- go/hayride/ai/models/repository/repository.go
+++ go/hayride/ai/models/repository/repository.go
@@ -7,11 +7,43 @@ import (
 )
 
 // Download downloads a model by its name and returns the path to the model file.
-func Download(modelName string) (string, error) {
-	modelResult := modelrepository.Download(modelName)
+func DownloadModel(modelName string) (string, error) {
+	modelResult := modelrepository.DownloadModel(modelName)
 	if modelResult.IsErr() {
 		return "", fmt.Errorf("failed to download model %s: %s", modelName, modelResult.Err().Data())
 	}
 
 	return *modelResult.OK(), nil
+}
+
+func GetModel(modelName string) (string, error) {
+	modelResult := modelrepository.GetModel(modelName)
+	if modelResult.IsErr() {
+		return "", fmt.Errorf("failed to get model %s: %s", modelName, modelResult.Err().Data())
+	}
+
+	return *modelResult.OK(), nil
+}
+
+func DeleteModel(modelName string) error {
+	deleteResult := modelrepository.DeleteModel(modelName)
+	if deleteResult.IsErr() {
+		return fmt.Errorf("failed to delete model %s: %s", modelName, deleteResult.Err().Data())
+	}
+
+	return nil
+}
+
+func ListModels() ([]string, error) {
+	modelsResult := modelrepository.ListModels()
+	if modelsResult.IsErr() {
+		return nil, fmt.Errorf("failed to list models: %s", modelsResult.Err().Data())
+	}
+
+	modelsList := modelsResult.OK()
+	if modelsList == nil {
+		return nil, fmt.Errorf("no models found")
+	}
+
+	return modelsList.Slice(), nil
 }
