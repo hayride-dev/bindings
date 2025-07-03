13ee725e3ee4e486ee37c241fb00e15fa9a7145c
Jeff Simpson <jeff1010322@gmail.com>
2025-07-03 10:09:35 -0400

    feat: update to coven v0.0.57, implement new model repository interface (#60)

--- go/internal/gen/hayride/silo/threads/threads.wasm.go
+++ go/internal/gen/hayride/silo/threads/threads.wasm.go
@@ -6,32 +6,32 @@ import (
 	"go.bytecodealliance.org/cm"
 )
 
-// This file contains wasmimport and wasmexport declarations for "hayride:silo@0.0.56".
+// This file contains wasmimport and wasmexport declarations for "hayride:silo@0.0.57".
 
-//go:wasmimport hayride:silo/threads@0.0.56 [resource-drop]thread
+//go:wasmimport hayride:silo/threads@0.0.57 [resource-drop]thread
 //go:noescape
 func wasmimport_ThreadResourceDrop(self0 uint32)
 
-//go:wasmimport hayride:silo/threads@0.0.56 [method]thread.id
+//go:wasmimport hayride:silo/threads@0.0.57 [method]thread.id
 //go:noescape
 func wasmimport_ThreadID(self0 uint32, result *cm.Result[string, string, ErrNo])
 
-//go:wasmimport hayride:silo/threads@0.0.56 [method]thread.wait
+//go:wasmimport hayride:silo/threads@0.0.57 [method]thread.wait
 //go:noescape
 func wasmimport_ThreadWait(self0 uint32, result *cm.Result[cm.List[uint8], cm.List[uint8], ErrNo])
 
-//go:wasmimport hayride:silo/threads@0.0.56 spawn
+//go:wasmimport hayride:silo/threads@0.0.57 spawn
 //go:noescape
 func wasmimport_Spawn(pkg0 *uint8, pkg1 uint32, function0 *uint8, function1 uint32, args0 *string, args1 uint32, result *cm.Result[Thread, Thread, ErrNo])
 
-//go:wasmimport hayride:silo/threads@0.0.56 status
+//go:wasmimport hayride:silo/threads@0.0.57 status
 //go:noescape
 func wasmimport_Status(id0 *uint8, id1 uint32, result *cm.Result[ThreadMetadataShape, ThreadMetadata, ErrNo])
 
-//go:wasmimport hayride:silo/threads@0.0.56 kill
+//go:wasmimport hayride:silo/threads@0.0.57 kill
 //go:noescape
 func wasmimport_Kill(id0 *uint8, id1 uint32, result *cm.Result[ErrNo, struct{}, ErrNo])
 
-//go:wasmimport hayride:silo/threads@0.0.56 group
+//go:wasmimport hayride:silo/threads@0.0.57 group
 //go:noescape
 func wasmimport_Group(result *cm.Result[cm.List[ThreadMetadata], cm.List[ThreadMetadata], ErrNo])
